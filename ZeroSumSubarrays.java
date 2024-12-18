abd6b058412e77e41278164c0e50f22567fde25e
Ayush Kumar <121686171+ay05h@users.noreply.github.com>
2024-12-18 12:28:15 +0530

    Update ZeroSumSubarrays.java

--- ZeroSumSubarrays.java
+++ ZeroSumSubarrays.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 class ZeroSumSubarrays {
-    public static int findSubarray(int[] arr) {
+    public static int findSubarraySumZero(int[] arr) {
         int count=0;
         HashMap<Integer, Integer> preSum=new HashMap<>();
         preSum.put(0, 1);
@@ -14,9 +14,24 @@ class ZeroSumSubarrays {
         }
         return count;
     }
+     public int subarraySumK(int[] nums, int k) {
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        int count = 0;
+        int sum = 0;
+        preSum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                count += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
   public static void main(String[] args ){
     int[] arr={6, -1, -3, 4, -2, 2, 4, 6, -12, -7};
-    int ans=findSubarray(arr);
+    int ans=findSubarraySumZero(arr);
     System.out.println("Number of subarrays with sum Zero is/are "+ans);
   }
 }
